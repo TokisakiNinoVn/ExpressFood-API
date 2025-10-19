e5084d0df5afe69649c4eeb4446f80a72a9dd270
Tokisaki Nino <trieuduytancbg@gmail.com>
2025-10-19 15:16:30 +0700

    .

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 connectDB();
 
 // Start server
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
